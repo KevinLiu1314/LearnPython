2f39159530c3b17f795a0f1b893927d68b52c2fb
Kevin Liu <kliu@home.com>
2014-01-10 19:51:11 -0500

    Clean Start, Repositories store locally at c:\Repositories

---
+++ Following Along.py
@@ -0,0 +1,13 @@
+# Comparison operators
+# >, <, ==, !=, >=, <=
+
+print("Hello World")
+
+n = 5
+
+if n > 5:
+    print("Greater than 5")
+elif n == 5:
+    print("Equal to 5")
+else:
+        print("Less than 5")
---
+++ Notes.py
@@ -0,0 +1,17 @@
+# Comparison operators
+# >, <, ==, !=, >=, <=
+
+print("Hello World")
+
+n = 5
+
+if n > 5:
+    print("Greater than 5")
+elif n == 5:
+    print("Equal to 5")
+else:
+    print("Less than 5")
+
+a = input("Enter a #: ")
+
+# 
